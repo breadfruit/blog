2bdc1df9acf3b635db57d2958f940cbe9c6efbc0
美式因子 <67834178+breadfruit@users.noreply.github.com>
2022-03-04 17:23:15 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,21 +4,6 @@ module.exports = {
   title: 'Study Of Toast',
   description: 'Just playing around',
   base:'/blog/',
-  head: [
-        // 设置 描述 和 关键词
-        [
-            "meta",
-            { name: "keywords", content: "react react-admin ant 后台管理系统" },
-        ],
-        [
-            "meta",
-            {
-                name: "description",
-                content:
-                    "此框架使用与二次开发，前端框架使用react，UI框架使用ant-design，全局数据状态管理使用redux，ajax使用库为axios。用于快速搭建中后台页面。",
-            },
-        ],
-  ],
   themeConfig: {
       //侧边栏
       sidebar: {
